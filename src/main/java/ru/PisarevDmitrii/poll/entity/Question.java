fda6fa42a55cd5d447c250cd02df5888dc5a4aa2
Pisarev <d.pisarev.03@mail.ru>
2022-02-21 22:33:19 +0300

    firth commit

--- src/main/java/ru/PisarevDmitrii/poll/entity/Question.java
+++ src/main/java/ru/PisarevDmitrii/poll/entity/Question.java
@@ -16,10 +16,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import java.util.List;
 
 @Entity
 @Table(name = "questions")
